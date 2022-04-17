adce90f58a6aa57df52075d9023b988f8c57bdaa
muthpeter <34248737+muthpeter@users.noreply.github.com>
2022-04-18 00:52:28 +0200

    added rudementary ordering by type to the table

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
    
    document.addEventListener('DOMContentLoaded',function(){
     
+    let fetchedData 
     
     let tableID = 0 
+    const proxyUrl ="http://localhost:3000/get"
+    fetch(proxyUrl)
+    .then(response => response.json())
+    .then(data => {
+    fetchedData = data
+    document.getElementById('data').innerText = "Fetching data via proxy from http://137.184.113.181:8001/admin/"
+
+    scribe()
+     
+    })
+  
+    //lists 10 element of the fetchedData array of objects
 
     let scribe = () => {
         for (i=0; i < 10; i++){
@@ -13,8 +26,8 @@
         }
         console.log(`tableID : ${tableID}`)
     }
-    const proxyUrl ="http://localhost:3000/get"
-    let fetchedData 
+    
+   
     document.getElementById('getData').onclick= () => {
         tableID = 0
         fetch(proxyUrl)
@@ -41,5 +54,72 @@
     };
     document.getElementById('forward').onclick = () =>{
         scribe()
+    }
+
+    document.getElementById('priceUp').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => a.price - b.price)
+        scribe()
+
+    }
+    document.getElementById('priceDown').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => b.price - a.price)
+        scribe()
+
+    }
+    document.getElementById('addressUp').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => {
+            let fa = a.address.toLowerCase(),
+            fb = b.address.toLowerCase();
+    
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+        }
+        )
+        scribe()
+
+    }
+    document.getElementById('addressDown').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => {
+            let fa = a.address.toLowerCase(),
+            fb = b.address.toLowerCase();
+    
+        if (fa > fb) {
+            return -1;
+        }
+        if (fa < fb) {
+            return 1;
+        }
+        return 0;
+        }
+        )
+        scribe()
+
+    }   
+    document.getElementById('idUp').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => a.id - b.id)
+        scribe()
+
+    }
+    document.getElementById('idDown').onclick = () =>{
+        //sort the array by adress, list the first 10
+        tableID = 0
+        fetchedData.sort((a,b) => b.id - b.id)
+        scribe()
+
     }
   });
